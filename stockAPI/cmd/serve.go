da295705b02b978cef1a27bc9da9ac9c47b2eda1
uthfol-sust <148445708+uthfol-sust@users.noreply.github.com>
2025-11-27 10:45:27 +0600

    stock api with postgresql

--- stockAPI/cmd/serve.go
+++ stockAPI/cmd/serve.go
@@ -9,23 +9,35 @@ import (
 	"stocksapi/pkg/repositories"
 	"stocksapi/pkg/router"
 	"stocksapi/pkg/services"
+	"time"
 )
 
 func Serve() {
-    db, err := connection.Connection()
+	db, err := connection.Connection()
 	if err != nil {
-	fmt.Println(err.Error())
-	  return
+		fmt.Println(err.Error())
+		return
 	}
-    
+
 	mux := http.NewServeMux()
 
 	createRepo := repositories.NewStocksRepository(db)
 	stockService := services.NewStocksService(createRepo)
-    stockController := controller.NewStocksController(stockService)
+	stockController := controller.NewStocksController(stockService)
 
-	router.Router(mux,stockController)
+	router.Router(mux, stockController)
+
+	
+	svr := http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		WriteTimeout: time.Second *30,
+		ReadTimeout: time.Second*10,
+		IdleTimeout: time.Minute,
+	}
+
+	
 
 	fmt.Println("Server Running on Port 8080")
-	log.Fatal(http.ListenAndServe(":8080",mux))
+	log.Fatal(svr.ListenAndServe())
 }
